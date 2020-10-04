daf0b1fa598dfe2a9bb6a9a0fed96ee362271225
kjmj <kmorton15@gmail.com>
2020-10-03 20:13:06 -0400

    checks for winner/draw, let the user restart the game

--- lib/board.dart
+++ lib/board.dart
@@ -37,17 +37,84 @@ class _Board extends State<Board> {
     );
   }
 
+  /// Updates the state of the board, changes the current player, and determines
+  /// if any dialogs should be shown
   void _boxTapped(index) {
     setState(() {
       if (board[index] == '') {
         if (currTurn == 'o') {
           board[index] = 'o';
-          currTurn = 'x';
+          if (!_winner()) {
+            currTurn = 'x';
+          }
         } else {
           board[index] = 'x';
-          currTurn = 'o';
+          if (!_winner()) {
+            currTurn = 'o';
+          }
         }
       }
     });
+    if (_winner()) {
+      _winDialog();
+    }
+    if (!board.contains('') && !_winner()) {
+      _drawDialog();
+    }
+  }
+
+  /// Returns true if there is a winner given the current state of the [board]
+  bool _winner() {
+    return (board[0] == board[1] && board[1] == board[2] && board[0] != '') ||
+        (board[3] == board[4] && board[4] == board[5] && board[3] != '') ||
+        (board[6] == board[7] && board[7] == board[8] && board[6] != '') ||
+        (board[0] == board[3] && board[3] == board[6] && board[0] != '') ||
+        (board[1] == board[4] && board[4] == board[7] && board[1] != '') ||
+        (board[2] == board[5] && board[5] == board[8] && board[2] != '') ||
+        (board[0] == board[4] && board[4] == board[8] && board[0] != '') ||
+        (board[2] == board[4] && board[4] == board[6] && board[2] != '');
+  }
+
+  /// Generic dialog to be shown at the end of a game containing [title]
+  /// This dialog will reset the board if they choose to play again
+  Future<void> _endOfGameDialog(String title) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Play again'),
+              onPressed: () {
+                _resetBoard();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  /// Dialog for when there is a winner
+  Future<void> _winDialog() async {
+    return _endOfGameDialog('Player ' + currTurn + ' has won!');
+  }
+
+  /// Dialog for when there is a draw
+  Future<void> _drawDialog() async {
+    return _endOfGameDialog('Draw, nobody won!');
+  }
+
+  /// Clears the board, resets the current player
+  void _resetBoard() {
+    for (int i = 0; i < board.length; i++) {
+      setState(() {
+        board[i] = '';
+      });
+    }
+    currTurn = 'o';
   }
 }
