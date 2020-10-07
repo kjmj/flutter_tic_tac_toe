e2ae92dd27634c1fa012b65cf283fc2c9f3d0a1f
kjmj <kmorton15@gmail.com>
2020-10-06 20:53:01 -0400

    fix and improve unit tests to use a mock widget

--- test/board_test.dart
+++ test/board_test.dart
@@ -2,16 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_tic_tac_toe/board.dart';
+import 'package:flutter_tic_tac_toe/board_model.dart';
+import 'package:provider/provider.dart';
+
+Widget mockBoard = MaterialApp(
+  home: InheritedProvider<BoardModel>(
+    create: (_) => BoardModel(),
+    child: Scaffold(
+      body: Board(),
+    ),
+  ),
+);
 
 void main() {
   testWidgets('board starts empty', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Board()));
+    await tester.pumpWidget(mockBoard);
 
     expect(find.text(''), findsNWidgets(9));
   });
 
   testWidgets('x is the starting player', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Board()));
+    await tester.pumpWidget(mockBoard);
     await tester.tap(find.byKey(Key('board_GestureDetector_index_0')));
     await tester.pump();
 
@@ -21,7 +32,7 @@ void main() {
   });
 
   testWidgets('x and o alternate turns', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Board()));
+    await tester.pumpWidget(mockBoard);
     await tester.tap(find.byKey(Key('board_GestureDetector_index_0')));
     await tester.pump();
 
@@ -36,7 +47,7 @@ void main() {
 
   testWidgets('tapping on a grid box that is occupied doesnt change the player',
       (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Board()));
+    await tester.pumpWidget(mockBoard);
     await tester.tap(find.byKey(Key('board_GestureDetector_index_0')));
     await tester.pump();
 
@@ -51,25 +62,11 @@ void main() {
 
   testWidgets('tapping on a grid box puts the mark in the correct spot',
       (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Board()));
+    await tester.pumpWidget(mockBoard);
     await tester.tap(find.byKey(Key('board_GestureDetector_index_5')));
     await tester.pump();
 
     Text text = tester.firstWidget(find.byKey(Key('board_Text_index_5')));
-    expect(text.data, 'x');
-  });
-
-  testWidgets('the weird one', (WidgetTester tester) async {
-    // // use custom screen size cuz flutter is weird
-    // tester.binding.window.physicalSizeTestValue = Size(1080, 1920);
-    // // resets the screen to its orinal size after the test end
-    // addTearDown(tester.binding.window.clearPhysicalSizeTestValue);
-
-    await tester.pumpWidget(MaterialApp(home: Board()));
-    await tester.tap(find.byKey(Key('board_GestureDetector_index_6')));
-    await tester.pump();
-
-    Text text = tester.firstWidget(find.byKey(Key('board_Text_index_6')));
     expect(text.data, 'x');
   });
 }
